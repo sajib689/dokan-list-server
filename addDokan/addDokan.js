8fd34368f789c97978ac3b5ffa29f539e9732e23
sajib689 <jibonraja11@gmail.com>
2024-09-28 17:56:09 +0600

    add the post method

--- addDokan/addDokan.js
+++ addDokan/addDokan.js
@@ -2,13 +2,18 @@ const express = require('express')
 const mongoose = require('mongoose');
 const router = express.Router();
 const addDokanSchema = require('../addDokanSchema/addDokanSchema.js')
-
+const Dokan = new mongoose.model('Dokan', addDokanSchema)
 
 router.get('/dokanlist', async(req, res) => {
     
 })
 router.post('/dokanlist', async(req, res) => {
-
+    try {
+        const newDokan = await Dokan(req.body)
+        res.status(200).json({message: 'dokan added successfully'})
+    } catch (err) {
+        console.log(err.message)
+    }
 })
 
 
